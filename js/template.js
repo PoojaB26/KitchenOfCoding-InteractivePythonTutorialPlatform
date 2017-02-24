4f90e4021973ccc4a62e36d2c76259dca18ab486
poojab26 <pbhaumik26@gmail.com>
2017-02-25 00:06:06 +0530

    JS work to check test cases

--- js/template.js
+++ js/template.js
@@ -1,6 +1,17 @@
 /**
  * Created by pblead26 on 24-Feb-17.
  */
+var el = document.getElementById("codeHere");
+var range = document.createRange();
+var sel = window.getSelection();
+range.setStart(el.childNodes[0], 0);
+range.collapse(true);
+sel.removeAllRanges();
+sel.addRange(range);
+
+
+
+
 function getCode(){
     var test=document.getElementById("test").innerHTML;
     var str = 'print "hello"';
