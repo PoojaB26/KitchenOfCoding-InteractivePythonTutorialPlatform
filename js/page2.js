fec9ab954177960195babe9ef1b63c36c598e48b
poojab26 <pbhaumik26@gmail.com>
2017-03-04 00:48:52 +0530

    added page2

---
+++ js/page2.js
@@ -0,0 +1,27 @@
+/**
+ * Created by pblead26 on 03-Mar-17.
+ */
+alert("hi");
+
+function checkConstant(){
+    var constOutput = document.getElementById("constantOutput");
+
+    var radios = document.getElementsByName('constant');
+
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            switch(i){
+                case 0: constOutput.innerHTML="But omelette doesnt need water.";
+                        break;
+                case 1: constOutput.innerHTML="We certainly don’t put salt in maggi, as far as I remember";
+                        break;
+                case 2: constOutput.innerHTML="That's correct";
+                        break;
+
+            }
+
+        }
+    }
+
+
+}
