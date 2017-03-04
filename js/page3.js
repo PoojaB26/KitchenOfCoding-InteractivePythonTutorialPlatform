a1f192c5496637b329bc4bf6f31d52ee230cb5ca
shrikriti5singh <shri5kriti.singh@gmail.com>
2017-03-04 19:13:20 +0530

    updated page3 added page3 css and page3 js

--- js/page3.js
+++ js/page3.js
@@ -1,3 +1,10 @@
 /**
  * Created by pblead26 on 04-Mar-17.
  */
+
+function showDiv() {
+    document.getElementById('solution').style.display = "block";
+}
+function showDiv2() {
+    document.getElementById('section2').style.display = "block";
+}
