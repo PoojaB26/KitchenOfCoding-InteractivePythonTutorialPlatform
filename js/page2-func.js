42e887f24e0fa35446b5aed9b538e32df42829eb
poojab26 <pbhaumik26@gmail.com>
2017-03-15 19:39:32 +0530

    Task Done: Page2 styling done.

--- js/page2-func.js
+++ js/page2-func.js
@@ -2,20 +2,23 @@
  * Created by pblead26 on 03-Mar-17.
  */
 var section2 = document.getElementById('section2');
+var constOutput = document.getElementById("constantOutput");
 
 function checkConstant(){
-    var constOutput = document.getElementById("constantOutput");
 
     var radios = document.getElementsByName('constant');
 
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
             switch(i){
-                case 0: constOutput.innerHTML="But omelette doesnt need water.";
+                case 0: showRemark();
+                    constOutput.innerHTML="But omelette doesnt need water.";
                     break;
-                case 1: constOutput.innerHTML="We certainly don’t put salt in maggi, as far as I remember";
+                case 1: showRemark();
+                    constOutput.innerHTML="We certainly don’t put salt in maggi, as far as I remember";
                     break;
-                case 2: constOutput.innerHTML="That's correct";
+                case 2: showRemark();
+                    constOutput.innerHTML="That's correct";
                     next();
                     break;
 
@@ -23,6 +26,10 @@ function checkConstant(){
 
         }
     }
+}
+
+function showRemark(){
+    constOutput.style.visibility = 'visible';
 }
 
 function next(){
