717afbe528c26362da7bff62cb4d1048db48223e
poojab26 <pbhaumik26@gmail.com>
2017-04-08 02:07:36 +0530

    Level 2 page 2 issue fixed

--- js/Level2-func/page2-func.js
+++ js/Level2-func/page2-func.js
@@ -1,40 +1,41 @@
 /**
  * Created by meow on 4/5/2017.
  */
-function findSelection(field) {
-    var test = document.getElementsByName(field);
-    var sizes = test.length;
 
-    //alert(sizes);
-    for (i=0; i < sizes; i++) {
-        if (test[i].checked==true) {
-            //alert(test[i].value + ' you got a value');
-            return test[i].value;
-        }
-    }
+function showRemark() {
+    radioOutput.style.visibility = 'visible';
+
 }
+function checkFunctionName(){
 
-function submitForm() {
+    var radios = document.getElementsByName('chooseFunc');
 
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            switch(i){
+                case 0: showRemark();
+                    radioOutput.innerHTML="Did we really use this yet?";
+                    break;
+                case 1: showRemark();
+                    radioOutput.innerHTML="Yes, you got it";
+                    goNext();
+                    break;
+                case 2: showRemark();
+                    radioOutput.innerHTML="Try again, maybe. We never used it before.";
+                    break;
 
-    var selection= findSelection("boil()");
-    if(selection=="yes"){
-        $(document).ready(function() {
-            $('#btnSun').click(myFunction);
-        });
-
-        function myFunction() {
-            document.getElementById('correct').style.visibility="visible";
-        }
             }
-    else {
-        $(document).ready(function () {
-            $('#btnSun').click(incorrect);
-        });
 
-        function incorrect() {
-            document.getElementById('incorrect').style.visibility = "visible";
         }
     }
-
 }
+
+
+function goNext() {
+    document.getElementById('GoNext').style.visibility = "visible";
+}
+
+function nextPage() {
+    $(function() {
+        $("#page2").load("page1.html");
+    });   }
